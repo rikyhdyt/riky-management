28400c5b12cbb7a8315badc616941cf466cef663
rikyhdyt <rikyhd015@gmail.com>
2022-12-20 15:30:04 +0700

    Tugas ke 27

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.css']
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit{
+
+  daftarFitur:any = [{
+    "title": "Product Management",
+    "image": "https://picsum.photos/200/90?random=3",
+    "link":""
+  }, 
+  {
+    "title": "User Management",
+    "image": "https://picsum.photos/200/90?random=2",
+    "link":""
+  }, 
+  {
+    "title": "Sales Management",
+    "image": "https://picsum.photos/200/90?random=1",
+    "link":""
+  },
+  {
+    "title": "Task Management",
+    "image": "https://picsum.photos/200/90?random=4",
+    "link":""
+  },
+  {
+    "title": "Content Management",
+    "image": "https://picsum.photos/200/90?random=5",
+    "link":""
+  }]
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
 
 }
